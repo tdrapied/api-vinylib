7b01c496d266a94df59cb2754d03c867e2e30459
tdrapied <t.drapied@gmail.com>
2022-07-21 13:26:34 +0200

    Add user entity + find one route

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly userRepository: UsersRepository) {}
+
+  async findOne(id: string): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: { id },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return new User(user);
+  }
+}
