022b628925864a902b7d2f90f802ddedbc629cae
tdrapied <t.drapied@gmail.com>
2023-01-05 14:38:58 +0100

    Add max length to dto

--- src/modules/vinyls/dto/search-vinyl-cover-query.dto.ts
+++ src/modules/vinyls/dto/search-vinyl-cover-query.dto.ts
@@ -2,8 +2,10 @@ import { IsString, MaxLength } from 'class-validator';
 
 export class SearchVinylCoverQueryDto {
   @IsString()
+  @MaxLength(250)
   name: string;
 
   @IsString()
+  @MaxLength(250)
   artist: string;
 }
